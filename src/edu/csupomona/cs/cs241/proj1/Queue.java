2bd5c83ed637bc4ca6331e64dafc666a8d22220e
Satshabad Khalsa <Satshabad.music@gmail.com>
2011-10-05 19:20:25 -0700

    Test commit

--- src/edu/csupomona/cs/cs241/proj1/Queue.java
+++ src/edu/csupomona/cs/cs241/proj1/Queue.java
@@ -1,10 +1,18 @@
 /**
- * 
+ * CS 241: Data Structures and Algorithms II
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Programming Assignment #N
+ *
+ * <description-of-assignment>
+ *
+ * Satshabad Khalsa  
  */
+
 package edu.csupomona.cs.cs241.proj1;
 
 /**
- * @author Satshabad
+ * @author Satshabad Khalsa
  *
  */
 public class Queue
