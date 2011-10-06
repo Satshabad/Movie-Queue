e885ab331d6088cd754cf7867ce2c081307a8233
Satshabad Khalsa <Satshabad.music@gmail.com>
2011-10-06 00:44:12 -0700

    Created Movie framework class

--- src/edu/csupomona/cs/cs241/proj1/Movie.java
+++ src/edu/csupomona/cs/cs241/proj1/Movie.java
@@ -17,5 +17,63 @@ package edu.csupomona.cs.cs241.proj1;
  */
 public class Movie
 {
+   private String name;
+   private String genre;
+   private String actorOne;
+   private String actorTwo;
+   private String actorThree;
+   
+   
+   /**
+    * Gets the name of the movie
+    * 
+    * @return name of the movie
+    */
+   public String getName()
+   {
+      return name;
+   }
+ 
+   /**
+    * Gets the genre of the movie
+    * 
+    * @return genre of the movie
+    */
+   public String getGenre()
+   {
+      return genre;
+   }
+   
+   /**
+    * Gets the name the first actor in the movie
+    * 
+    *  @return name of the actor
+    */
+   public String getActorOne()
+   {
+      return actorOne;
+   }
+   
+   /**
+    * Gets the name the second actor in the movie
+    * 
+    *  @return name of the actor
+    */
+   public String getActorTwo()
+   {
+      return actorTwo;
+   }
+   
+   /**
+    * Gets the name the third actor in the movie
+    * 
+    *  @return name of the actor
+    */
+   public String getActorThree()
+   {
+      return actorThree;
+   }
 
+   
+   
 }
