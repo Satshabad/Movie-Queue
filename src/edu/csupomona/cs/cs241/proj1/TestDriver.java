74eb441ca28e9d37f50f14ee39f02f76db3bce95
Satshabad Khalsa <Satshabad.music@gmail.com>
2011-10-06 00:43:42 -0700

    Found file from File constructor

--- src/edu/csupomona/cs/cs241/proj1/TestDriver.java
+++ src/edu/csupomona/cs/cs241/proj1/TestDriver.java
@@ -1,38 +1,21 @@
 package edu.csupomona.cs.cs241.proj1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TestDriver
 {
 
    /**
     * @param args
+    * @throws FileNotFoundException 
     */
-   public static void main(String[] args)
+   public static void main(String[] args) throws FileNotFoundException
    {
-      PriorityQueue<String> q = new PriorityQueue<String>(3, 8);
-      System.err.println(q.enqueue("one", 1));
-      System.err.println(q.enqueue("two", 3));
-      System.err.println(q.enqueue("three", 1));
-      System.err.println(q.enqueue("four", 1));
-      System.err.println(q.enqueue("five", 2));
-      System.err.println(q.enqueue("six", 2));
-      System.err.println(q.enqueue("seven", 3));
-      System.err.println(q.enqueue("eight", 1));
-      System.err.println(q.enqueue("nine", 3));
-      System.err.println(q.enqueue("ten", 2));
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      System.err.println(q.dequeue());
-      
+    File movieFileList = new File("res" +File.separator +"movies.txt");  
+     Scanner scan = new Scanner(movieFileList);
+
    }
 
 }
