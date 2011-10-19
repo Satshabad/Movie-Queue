81099b4dfddc5ccb4d204abff6d45942b2e50047
Satshabad Khalsa <Satshabad.music@gmail.com>
2011-10-18 19:39:29 -0700

    Finalized javadoc and format of DeepCopy

--- src/edu/csupomona/cs/cs241/proj1/DeepCopier.java
+++ src/edu/csupomona/cs/cs241/proj1/DeepCopier.java
@@ -7,71 +7,67 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-public class DeepCopier
-{
+/**
+ * This utility class contains one method to make a deep copy of a
+ * {@link Serializable} object
+ * 
+ * @author Satshabad
+ * 
+ */
+public class DeepCopier {
 
-   public static Serializable copy(Serializable tobeCopied){
-      Serializable copy = null; 
-      ByteArrayOutputStream bos = new ByteArrayOutputStream();
-      ObjectOutputStream out= null;;
-      try
-      {
-         out = new ObjectOutputStream(bos);
-      } catch (IOException e)
-      {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      }
-      try
-      {
-         out.writeObject(tobeCopied);
-      } catch (IOException e)
-      {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      }
-      try
-      {
-         out.flush();
-      } catch (IOException e)
-      {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      }
-      try
-      {
-         out.close();
-      } catch (IOException e)
-      {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      }
+    /**
+     * This method takes an object and returns a copy of it with no references
+     * connecting them.
+     * 
+     * @pre the passed object must not be null
+     * @post the object returned has no connection with the passed object
+     * @param tobeCopied
+     *            the object to be copies.
+     * @return an object that is a copy of the passed object but shares no
+     *         reference with it.
+     */
+    public static Serializable copy(Serializable tobeCopied) {
+        Serializable copy = null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(bos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.writeObject(tobeCopied);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-      ObjectInputStream in = null;
-      try
-      {
-         in = new ObjectInputStream(
-             new ByteArrayInputStream(bos.toByteArray()));
-      } catch (IOException e)
-      {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      }
-      try
-      {
-         copy = (Serializable) in.readObject();
-      } catch (IOException e)
-      {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      } catch (ClassNotFoundException e)
-      {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      }
-      return copy;
-      
-      
-   }
-   
+        ObjectInputStream in = null;
+        try {
+            in = new ObjectInputStream(new ByteArrayInputStream(
+                    bos.toByteArray()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            copy = (Serializable) in.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return copy;
+
+    }
+
 }
