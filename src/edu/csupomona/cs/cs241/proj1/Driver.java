41540161a42f812b112e7667e52c17080c518df0
Satshabad Khalsa <Satshabad.music@gmail.com>
2011-10-18 21:27:08 -0700

    Transfered execution to the Driver class

---
+++ src/edu/csupomona/cs/cs241/proj1/Driver.java
@@ -0,0 +1,20 @@
+
+package edu.csupomona.cs.cs241.proj1;
+
+import java.io.IOException;
+
+import edu.csupomona.cs.cs241.proj1gui.GuiFrame;
+
+/**
+ * 
+ * @author Satshabad
+ */
+public class Driver {
+
+    public static void main(String args[]){
+
+        GuiFrame.gui();
+
+
+    }
+}
--- src/edu/csupomona/cs/cs241/proj1gui/GuiFrame.java
+++ src/edu/csupomona/cs/cs241/proj1gui/GuiFrame.java
@@ -798,7 +798,7 @@ public class GuiFrame extends javax.swing.JFrame {
     /**
     * @param args the command line arguments
     */
-    public static void main(String args[]) {
+    public static void gui() {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
             try
