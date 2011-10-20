196815b128d875c487cc80626d6a2553ad212773
Satshabad Khalsa <Satshabad.music@gmail.com>
2011-10-20 13:46:55 -0700

    Merged the gui package with the back end package as per professors specifications

---
+++ README.txt
@@ -0,0 +1,20 @@
+I cannot say for sure that this program has no bugs I can only 
+hope you don't find them. BUT if you do and the program crashes 
+and won't start back up, you need to delete the "homeQueue.txt" 
+and "waitingQueue.txt" from the /sav file and restart. This will 
+kill off any persistent queues.
+
+To add movies to the library, open the "movies.txt" file and add 
+attributes in this order:
+
+Title
+Genre
+Main actor
+another actor
+another actor
+Director
+\n (newline)
+
+make sure there is only exactly one new line after each movie 
+especially at the end of the file.
+
---
+++ src/edu/csupomona/cs/cs241/proj1/CustomListRenderer.java
@@ -0,0 +1,58 @@
+/**
+ * CS 241: Data Structures and Algorithms II
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Programming Assignment 1
+ *
+ * This assignment is to create a netflix like movie
+ * 
+ * queue by which the user can sort search a
+ * library of movies as well as add movies to
+ * their queue by priority rather than just
+ * FIFO order. They may freely switch
+ * between FIFO and priority order. The
+ * Queues are persistent and the library is extensible.
+ * 
+ * Satshabad Khalsa  
+ */
+
+
+package edu.csupomona.cs.cs241.proj1;
+
+import edu.csupomona.cs.cs241.proj1.Movie;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+import javax.swing.ListCellRenderer;
+
+/**
+ *
+ * @author Satshabad
+ */
+public class CustomListRenderer implements ListCellRenderer {
+
+   @Override
+   public Component getListCellRendererComponent(JList list, Object value, int index,
+        boolean isSelected, boolean cellHasFocus) {
+
+        Movie theMovie = (Movie) value;
+        JTextArea renderer = new JTextArea(7,10);
+        renderer.setWrapStyleWord(true);
+        renderer.setLineWrap(true);
+        renderer.addMouseListener(null);
+        renderer.append(theMovie.getTitle() +"\n");
+        renderer.append("Director: " +theMovie.getDirector() +"\n");
+        renderer.append("Actors: " + theMovie.getActorOne() +", "
+                        + theMovie.getActorTwo() + ", "
+                        + theMovie.getActorThree() + "\n");
+        renderer.append("Genre: " + theMovie.getGenre()+ "\n");
+        renderer.append("-----------------------------------------------------");
+        renderer.setLineWrap(true);
+        if (isSelected){renderer.setBackground(Color.LIGHT_GRAY);}
+
+         
+
+        return renderer;
+   }
+}
